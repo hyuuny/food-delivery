627a5ab6a1b3cef09496c761f3c7608f611d178f
hyuuny <shyune@knou.ac.kr>
2024-05-16 22:18:14 +0900

    test(use-case): UpdateMenuUseCaseTest 추가
    - UpdateMenuUseCaseTest 클래스 추가
      - 메뉴를 수정할 때 금액이 0 이상인 경우와 0 이하인 경우에 대한 테스트 케이스 추가

---
+++ src/test/kotlin/hyuuny/fooddelivery/application/menu/UpdateMenuUseCaseTest.kt
@@ -0,0 +1,67 @@
+package hyuuny.fooddelivery.application.menu
+
+import UpdateMenuRequest
+import hyuuny.fooddelivery.domain.menu.Menu
+import hyuuny.fooddelivery.domain.menu.MenuStatus
+import hyuuny.fooddelivery.domain.menu.Price
+import hyuuny.fooddelivery.infrastructure.menu.MenuRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import java.time.LocalDateTime
+
+internal class UpdateMenuUseCaseTest : BehaviorSpec({
+
+    val repository = mockk<MenuRepository>()
+    val useCase = MenuUseCase(repository)
+
+    given("메뉴를 수정 할 때") {
+        val menuId = 1L
+        val now = LocalDateTime.now()
+        val existingMenu = Menu(
+            id = menuId,
+            name = "싸이버거",
+            price = Price(6000),
+            status = MenuStatus.ON_SALE,
+            popularity = true,
+            imageUrl = "cyburger-image-url",
+            description = "[베스트]닭다리살",
+            createdAt = now,
+            updatedAt = now
+        )
+        val request = UpdateMenuRequest(
+            name = "후라이드 치킨",
+            price = 20000,
+            popularity = false,
+            imageUrl = "chicken-image-url",
+            description = "맛있는 양념치킨"
+        )
+        coEvery { repository.findById(any()) } returns existingMenu
+        coEvery { repository.update(any()) } returns Unit
+
+        `when`("금액을 0이상으로 입력하면") {
+            useCase.updateMenu(menuId, request)
+            then("메뉴를 수정 할 수 있다.") {
+                coVerify { repository.update(any()) }
+            }
+        }
+
+        `when`("금액을 0원 이하로 입력하면") {
+            coEvery { repository.findById(any()) } returns existingMenu
+
+            then("메뉴를 수정 할 수 없다.") {
+                shouldThrow<IllegalArgumentException> {
+                    useCase.updateMenu(menuId, UpdateMenuRequest(
+                        name = "후라이드 치킨",
+                        price = 0,
+                        popularity = false,
+                        imageUrl = "chicken-image-url",
+                        description = "맛있는 양념치킨"
+                    ))
+                }
+            }
+        }
+    }
+})
