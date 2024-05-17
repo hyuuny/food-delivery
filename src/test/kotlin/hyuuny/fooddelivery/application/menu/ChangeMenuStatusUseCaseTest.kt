aee2498be38341686a9d844a9a457aebac156e71
hyuuny <shyune@knou.ac.kr>
2024-05-17 20:33:34 +0900

    test(use-case): ChangeMenuStatusUseCaseTest 추가
    - ChangeMenuStatusUseCaseTest 클래스 추가
      - 존재하는 메뉴의 상태를 정상적으로 변경할 수 있는지 테스트
      - 존재하지 않는 메뉴에 대해 상태 변경 시 IllegalStateException이 발생하는지 테스트

---
+++ src/test/kotlin/hyuuny/fooddelivery/application/menu/ChangeMenuStatusUseCaseTest.kt
@@ -0,0 +1,61 @@
+package hyuuny.fooddelivery.application.menu
+
+import ChangeMenuStatusRequest
+import hyuuny.fooddelivery.domain.menu.Menu
+import hyuuny.fooddelivery.domain.menu.MenuStatus
+import hyuuny.fooddelivery.domain.menu.Price
+import hyuuny.fooddelivery.infrastructure.menu.MenuRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import java.time.LocalDateTime
+
+internal class ChangeMenuStatusUseCaseTest : BehaviorSpec({
+
+    val repository = mockk<MenuRepository>()
+    val useCase = MenuUseCase(repository)
+
+    given("메뉴의 상태를 변경 할 때") {
+        val menuId = 1L
+        val now = LocalDateTime.now()
+        val existingMenu = Menu(
+            id = menuId,
+            name = "싸이버거",
+            price = Price(6000),
+            status = MenuStatus.ON_SALE,
+            popularity = true,
+            imageUrl = "cyburger-image-url",
+            description = "[베스트]닭다리살",
+            createdAt = now,
+            updatedAt = now
+        )
+        val request = ChangeMenuStatusRequest(
+            status = MenuStatus.SOLD_OUT
+        )
+        coEvery { repository.findById(any()) } returns existingMenu
+        coEvery { repository.updateMenuStatus(any()) } returns Unit
+
+        `when`("존재하는 메뉴라면") {
+            useCase.changeMenuStatus(menuId, request)
+
+            then("메뉴의 상태를 변경 할 수 있다.") {
+                coVerify { repository.updateMenuStatus(any()) }
+            }
+        }
+
+        `when`("존재하지 않는 메뉴라면") {
+            coEvery { repository.findById(any()) } returns null
+
+            then("메뉴의 상태를 변경 할 수 없다.") {
+                val notExistId = 0L
+                val ex = shouldThrow<IllegalStateException> {
+                    useCase.changeMenuStatus(notExistId, ChangeMenuStatusRequest(MenuStatus.SOLD_OUT))
+                }
+                ex.message shouldBe "${notExistId}번 메뉴를 찾을 수 없습니다."
+            }
+        }
+    }
+})
