9e7ea86333834954189a1ce396529b68bc0f6c12
hyuuny <shyune@knou.ac.kr>
2024-05-20 19:38:17 +0900

    test(use-case): GetMenuGroupUseCaseTest 추가
    - 존재하는 메뉴 그룹 ID로 메뉴 그룹을 성공적으로 조회하는지 테스트.
    - 조회된 메뉴 그룹이 올바른 세부 정보를 포함하는지 검증: id, menuId, name, required, createdAt, updatedAt.
    - 존재하지 않는 메뉴 그룹 ID에 대한 처리를 테스트하여, 적절한 메시지와 함께 IllegalStateException이 발생하는지 확인.

---
+++ src/test/kotlin/hyuuny/fooddelivery/application/menugroup/GetMenuGroupUseCaseTest.kt
@@ -0,0 +1,55 @@
+package hyuuny.fooddelivery.application.menugroup
+
+import hyuuny.fooddelivery.domain.menugroup.MenuGroup
+import hyuuny.fooddelivery.infrastructure.menugroup.MenuGroupRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import java.time.LocalDateTime
+
+internal class GetMenuGroupUseCaseTest : BehaviorSpec({
+
+    val repository = mockk<MenuGroupRepository>()
+    val useCase = MenuGroupUseCase(repository)
+
+    given("메뉴그룹을 상세조회 할 때") {
+        val expectedMenuId = 5L
+        val expectedMenuGroupId = 1L
+        val now = LocalDateTime.now()
+        val expectedMenuGroup = MenuGroup(
+            id = expectedMenuGroupId,
+            menuId = expectedMenuId,
+            name = "반반치킨 선택",
+            required = true,
+            createdAt = now,
+            updatedAt = now
+        )
+        coEvery { repository.findById(any()) } returns expectedMenuGroup
+
+        `when`("존재하는 아이디이면") {
+            val result = useCase.getMenuGroup(expectedMenuGroupId)
+
+            then("메뉴를 상세조회 할 수 있다.") {
+                result.id shouldBe expectedMenuGroupId
+                result.menuId shouldBe expectedMenuId
+                result.name shouldBe expectedMenuGroup.name
+                result.required shouldBe expectedMenuGroup.required
+                result.createdAt shouldBe expectedMenuGroup.createdAt
+                result.updatedAt shouldBe expectedMenuGroup.updatedAt
+            }
+        }
+
+        `when`("존재하지 않는 아이디이면") {
+            coEvery { repository.findById(any()) } returns null
+
+            then("메뉴를 상세조회 할 수 없다.") {
+                val ex = shouldThrow<IllegalStateException> {
+                    useCase.getMenuGroup(0)
+                }
+                ex.message shouldBe "0번 메뉴그룹을 찾을 수 없습니다."
+            }
+        }
+    }
+})
