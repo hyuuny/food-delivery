fd97a517a9350a13bfea03fb99b1628b4b94ae61
hyuuny <shyune@knou.ac.kr>
2024-06-02 16:50:34 +0900

    test(admin-api): 매장 조회 및 검색 기능 테스트 추가

--- src/test/kotlin/hyuuny/fooddelivery/presentation/admin/v1/store/StoreHandlerTest.kt
+++ src/test/kotlin/hyuuny/fooddelivery/presentation/admin/v1/store/StoreHandlerTest.kt
@@ -18,6 +18,9 @@ import hyuuny.fooddelivery.presentation.admin.v1.BaseIntegrationTest
 import io.mockk.coEvery
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.MediaType
 import java.time.LocalDateTime
 
@@ -181,6 +184,171 @@ class StoreHandlerTest : BaseIntegrationTest() {
             .jsonPath("$.storeImages[2].imageUrl").isEqualTo(storeImages[2].imageUrl)
             .jsonPath("$.createdAt").exists()
             .jsonPath("$.updatedAt").exists()
+    }
+
+    @DisplayName("매장 목록을 조회할 수 있다.")
+    @Test
+    fun getStores() {
+        val now = LocalDateTime.now()
+        val firstStore = Store(
+            id = 1L,
+            categoryId = 1L,
+            deliveryType = DeliveryType.OUTSOURCING,
+            name = "BBQ",
+            ownerName = "김성현",
+            taxId = "123-12-12345",
+            deliveryFee = 1000,
+            minimumOrderAmount = 18000,
+            iconImageUrl = "icon-image-url.jpg",
+            description = "저희 업소는 100% 국내산 닭고기를 사용하며, BBQ 올리브 오일만을 사용합니다.",
+            foodOrigin = "황금올리브치킨(후라이드/속안심/핫윙/블랙페퍼/레드착착/크런치 버터), 핫황금올리브치킨크리스피, 파더`s치킨(로스트 갈릭/와사비)",
+            phoneNumber = "02-1234-1234",
+            createdAt = now,
+            updatedAt = now,
+        )
+
+        val secondStore = Store(
+            id = 2L,
+            categoryId = 2L,
+            deliveryType = DeliveryType.OUTSOURCING,
+            name = "백종원의 빽보이피자",
+            ownerName = "나피자",
+            taxId = "125-21-38923",
+            deliveryFee = 0,
+            minimumOrderAmount = 14000,
+            iconImageUrl = "icon-image-url-1.jpg",
+            description = "안녕하세요. 백종원이 빽보이피자입니다 :)\n" +
+                    " ★ 음료는 기본 제공되지 않습니다. 필요하신분은 추가 주문 부탁드립니다.\n" +
+                    " ★ 다양한 리뷰이베트는 리뷰칸을 확인해주세요!",
+            foodOrigin = "슈퍼빽보이'카나디언:돼지고기(국내산), 페퍼로니: 돼지고기(국내산과 외국산 섞음), 소고기(호주산), 베이컨:돼지고기(미국산)",
+            phoneNumber = "02-1231-2308",
+            createdAt = now.plusHours(1),
+            updatedAt = now.plusHours(1),
+        )
+
+        val thirdStore = Store(
+            id = 3L,
+            categoryId = 2L,
+            deliveryType = DeliveryType.OUTSOURCING,
+            name = "백종원의 빽보이피자 2",
+            ownerName = "다피자",
+            taxId = "125-21-09283",
+            deliveryFee = 1000,
+            minimumOrderAmount = 13000,
+            iconImageUrl = "icon-image-url-2.jpg",
+            description = "안녕하세요. 백종원이 빽보이피자 2입니다 :)\n" +
+                    " ★ 음료는 기본 제공되지 않습니다. 필요하신분은 추가 주문 부탁드립다.\n" +
+                    " ★ 다양한 리뷰이베트는 리뷰칸을 확인해주세요!",
+            foodOrigin = "슈퍼빽보이'카나디언:돼지고기(국내산), 페퍼로니: 돼지고기(국내산과 외국산 섞음), 소고기(호주산), 베이컨:돼지고기(미국산)",
+            phoneNumber = "02-1938-2973",
+            createdAt = now.plusHours(3),
+            updatedAt = now.plusHours(3),
+        )
+
+        val fourthStore = Store(
+            id = 4L,
+            categoryId = 2L,
+            deliveryType = DeliveryType.TAKE_OUT,
+            name = "가종원의 빽보이피자",
+            ownerName = "가피자",
+            taxId = "125-21-12397",
+            deliveryFee = 3000,
+            minimumOrderAmount = 15000,
+            description = "안녕하세요. 가종원이 빽보이피자입니다 :)\n" +
+                    " ★ 음료는 기본 제공되지 않습니다. 필요하신분은 추가 주문 부탁드립니다.\n" +
+                    " ★ 다양한 리뷰이베트는 리뷰칸을 확인해주세요!",
+            foodOrigin = "슈퍼빽보이'카나디언:돼지고기(국내산), 페퍼로니: 돼지고기(국내산과 외국산 섞음), 소고기(호주산), 베이컨:돼지고기(미국산)",
+            phoneNumber = "02-1726-2397",
+            createdAt = now.plusHours(4),
+            updatedAt = now.plusHours(4),
+        )
+
+        val fifthStore = Store(
+            id = 5L,
+            categoryId = 2L,
+            deliveryType = DeliveryType.SELF,
+            name = "가종원의 빽보이피자 2",
+            ownerName = "기피자",
+            taxId = "125-21-38723",
+            deliveryFee = 2500,
+            minimumOrderAmount = 16000,
+            description = "안녕하세요. 가종원이 빽보이피자 2입니다 :)\n" +
+                    " ★ 음료는 기본 제공되지 않습니다. 필요하신분은 추가 주문 부탁드립니다.\n" +
+                    " ★ 다양한 리뷰이베트는 리뷰칸을 확인해주세요!",
+            foodOrigin = "슈퍼빽보이'카나디언:돼지고기(국내산), 페퍼로니: 돼지고기(국내산과 외국산 섞음), 소고기(호주산), 베이컨:돼지고기(미국산)",
+            phoneNumber = "070-9278-8765",
+            createdAt = now.plusHours(5),
+            updatedAt = now.plusHours(5),
+        )
+        val stores = listOf(firstStore, secondStore, thirdStore, fourthStore, fifthStore).sortedByDescending { it.id }
+
+        val pageable = PageRequest.of(0, 15, Sort.by(Sort.DEFAULT_DIRECTION, "id"))
+        val page = PageImpl(stores, pageable, stores.size.toLong())
+        coEvery { useCase.getStores(any(), any()) } returns page
+
+        webTestClient.get().uri("/admin/v1/stores?sort:id:desc")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .consumeWith(::println)
+            .jsonPath("$.content[0].id").isEqualTo(fifthStore.id!!)
+            .jsonPath("$.content[0].categoryId").isEqualTo(fifthStore.categoryId)
+            .jsonPath("$.content[0].deliveryType").isEqualTo(fifthStore.deliveryType.name)
+            .jsonPath("$.content[0].name").isEqualTo(fifthStore.name)
+            .jsonPath("$.content[0].ownerName").isEqualTo(fifthStore.ownerName)
+            .jsonPath("$.content[0].taxId").isEqualTo(fifthStore.taxId)
+            .jsonPath("$.content[0].deliveryFee").isEqualTo(fifthStore.deliveryFee)
+            .jsonPath("$.content[0].phoneNumber").isEqualTo(fifthStore.phoneNumber)
+            .jsonPath("$.content[0].createdAt").exists()
+
+            .jsonPath("$.content[1].id").isEqualTo(fourthStore.id!!)
+            .jsonPath("$.content[1].categoryId").isEqualTo(fourthStore.categoryId)
+            .jsonPath("$.content[1].deliveryType").isEqualTo(fourthStore.deliveryType.name)
+            .jsonPath("$.content[1].name").isEqualTo(fourthStore.name)
+            .jsonPath("$.content[1].ownerName").isEqualTo(fourthStore.ownerName)
+            .jsonPath("$.content[1].taxId").isEqualTo(fourthStore.taxId)
+            .jsonPath("$.content[1].deliveryFee").isEqualTo(fourthStore.deliveryFee)
+            .jsonPath("$.content[1].phoneNumber").isEqualTo(fourthStore.phoneNumber)
+            .jsonPath("$.content[1].createdAt").exists()
+
+            .jsonPath("$.content[2].id").isEqualTo(thirdStore.id!!)
+            .jsonPath("$.content[2].categoryId").isEqualTo(thirdStore.categoryId)
+            .jsonPath("$.content[2].deliveryType").isEqualTo(thirdStore.deliveryType.name)
+            .jsonPath("$.content[2].name").isEqualTo(thirdStore.name)
+            .jsonPath("$.content[2].ownerName").isEqualTo(thirdStore.ownerName)
+            .jsonPath("$.content[2].taxId").isEqualTo(thirdStore.taxId)
+            .jsonPath("$.content[2].deliveryFee").isEqualTo(thirdStore.deliveryFee)
+            .jsonPath("$.content[2].iconImageUrl").isEqualTo(thirdStore.iconImageUrl!!)
+            .jsonPath("$.content[2].phoneNumber").isEqualTo(thirdStore.phoneNumber)
+            .jsonPath("$.content[2].createdAt").exists()
+
+            .jsonPath("$.content[3].id").isEqualTo(secondStore.id!!)
+            .jsonPath("$.content[3].categoryId").isEqualTo(secondStore.categoryId)
+            .jsonPath("$.content[3].deliveryType").isEqualTo(secondStore.deliveryType.name)
+            .jsonPath("$.content[3].name").isEqualTo(secondStore.name)
+            .jsonPath("$.content[3].ownerName").isEqualTo(secondStore.ownerName)
+            .jsonPath("$.content[3].taxId").isEqualTo(secondStore.taxId)
+            .jsonPath("$.content[3].deliveryFee").isEqualTo(secondStore.deliveryFee)
+            .jsonPath("$.content[3].iconImageUrl").isEqualTo(secondStore.iconImageUrl!!)
+            .jsonPath("$.content[3].phoneNumber").isEqualTo(secondStore.phoneNumber)
+            .jsonPath("$.content[3].createdAt").exists()
+
+            .jsonPath("$.content[4].id").isEqualTo(firstStore.id!!)
+            .jsonPath("$.content[4].categoryId").isEqualTo(firstStore.categoryId)
+            .jsonPath("$.content[4].deliveryType").isEqualTo(firstStore.deliveryType.name)
+            .jsonPath("$.content[4].name").isEqualTo(firstStore.name)
+            .jsonPath("$.content[4].ownerName").isEqualTo(firstStore.ownerName)
+            .jsonPath("$.content[4].taxId").isEqualTo(firstStore.taxId)
+            .jsonPath("$.content[4].deliveryFee").isEqualTo(firstStore.deliveryFee)
+            .jsonPath("$.content[4].iconImageUrl").isEqualTo(firstStore.iconImageUrl!!)
+            .jsonPath("$.content[4].phoneNumber").isEqualTo(firstStore.phoneNumber)
+            .jsonPath("$.content[4].createdAt").exists()
+
+            .jsonPath("$.pageNumber").isEqualTo(1)
+            .jsonPath("$.size").isEqualTo(15)
+            .jsonPath("$.last").isEqualTo(true)
+            .jsonPath("$.totalElements").isEqualTo(5)
     }
 
     private fun generateStore(request: CreateStoreRequest, now: LocalDateTime): Store {
