e70f76da30a60a3dd306d47337ae30edd7560630
hyuuny <shyune@knou.ac.kr>
2024-06-14 19:21:47 +0900

    test(use-case): 회원 휴대폰 번호, 이미지 변경 및 탈퇴 기능 테스트 추가

--- src/test/kotlin/hyuuny/fooddelivery/application/user/ChangePhoneNumberUseCaseTest.kt
+++ src/test/kotlin/hyuuny/fooddelivery/application/user/ChangePhoneNumberUseCaseTest.kt
@@ -1,6 +1,6 @@
 package hyuuny.fooddelivery.application.user
 
-import ChangeUserEmailRequest
+import ChangeUserPhoneNumberRequest
 import hyuuny.fooddelivery.domain.user.User
 import hyuuny.fooddelivery.infrastructure.user.UserRepository
 import io.kotest.assertions.throwables.shouldThrow
@@ -10,13 +10,13 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import java.time.LocalDateTime
 
-class ChangeEmailUseCaseTest : BehaviorSpec({
+class ChangePhoneNumberUseCaseTest : BehaviorSpec({
 
     val repository = mockk<UserRepository>()
     val useCase = UserUseCase(repository)
     val verifier = mockk<UserVerifier>()
 
-    Given("회원이 자신의 이메일을 변경 할 때") {
+    Given("회원이 자신의 휴대폰 번호를 변경 할 때") {
         val id = 1L
         val now = LocalDateTime.now()
         val user = User(
@@ -29,27 +29,28 @@ class ChangeEmailUseCaseTest : BehaviorSpec({
             createdAt = now,
             updatedAt = now,
         )
-        val request = ChangeUserEmailRequest(email = "hyuuny@naver.com")
+        val request = ChangeUserPhoneNumberRequest(phoneNumber = "010-1234-5678")
         coEvery { repository.findById(any()) } returns user
-        coEvery { repository.updateEmail(any()) } returns Unit
+        coEvery { repository.updatePhoneNumber(any()) } returns Unit
 
         `when`("유효한 값이면") {
-            useCase.changeEmail(id, request)
+            useCase.changePhoneNumber(id, request)
 
-            then("정상적으로 이메일을 변경할 수 있다.") {
-                coEvery { repository.updateEmail(any()) }
+            then("정상적으로 휴대폰 번호를 변경할 수 있다.") {
+                coEvery { repository.updatePhoneNumber(any()) }
             }
         }
 
-        When("올바르지 않은 이메일 형식이면") {
-            val invalidRequest = ChangeUserEmailRequest(email = "hyuuny@naver123")
-            coEvery { verifier.verifyEmail(invalidRequest.email) } throws IllegalArgumentException("올바른 이메일 형식이 아닙니다. email: ${invalidRequest.email}")
+        When("올바르지 않은 휴대폰 번호 형식이면") {
+            val invalidRequest = ChangeUserPhoneNumberRequest(phoneNumber = "010-1234-567")
+            coEvery { verifier.verifyPhoneNumber(invalidRequest.phoneNumber) } throws
+                    IllegalArgumentException("올바른 휴대폰 번호 형식이 아닙니다. phoneNumber: ${invalidRequest.phoneNumber}")
 
-            Then("닉네임을 변경 할 수 없다.") {
+            Then("휴대폰 번호를 변경 할 수 없다.") {
                 val ex = shouldThrow<IllegalArgumentException> {
-                    useCase.changeEmail(id, invalidRequest)
+                    useCase.changePhoneNumber(id, invalidRequest)
                 }
-                ex.message shouldBe "올바른 이메일 형식이 아닙니다. email: ${invalidRequest.email}"
+                ex.message shouldBe "올바른 휴대폰 번호 형식이 아닙니다. phoneNumber: ${invalidRequest.phoneNumber}"
             }
         }
     }
