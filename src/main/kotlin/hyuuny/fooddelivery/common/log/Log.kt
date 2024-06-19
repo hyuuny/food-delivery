0a901d733dff1f4804f97186837e5d462542f9b8
hyuuny <shyune@knou.ac.kr>
2024-06-19 11:56:00 +0900

    feat(common): Log interface 추가

---
+++ src/main/kotlin/hyuuny/fooddelivery/common/log/Log.kt
@@ -0,0 +1,11 @@
+package hyuuny.fooddelivery.common.log
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+interface Log {
+
+    val log: Logger get() = LoggerFactory.getLogger(this.javaClass)
+
+}
+
