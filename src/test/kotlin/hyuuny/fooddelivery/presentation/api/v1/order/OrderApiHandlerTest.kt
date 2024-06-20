0d4f497f2e433ce0cef8bf81c1c86f1a94447226
hyuuny <shyune@knou.ac.kr>
2024-06-20 19:42:08 +0900

    test(api): 주문 상세조회 기능 테스트 추가

--- src/test/kotlin/hyuuny/fooddelivery/presentation/api/v1/order/OrderApiHandlerTest.kt
+++ src/test/kotlin/hyuuny/fooddelivery/presentation/api/v1/order/OrderApiHandlerTest.kt
@@ -132,6 +132,95 @@ class OrderApiHandlerTest : BaseIntegrationTest() {
             .jsonPath("$.totalPrice").isEqualTo(expectedResponse.totalPrice)
     }
 
+    @DisplayName("회원은 주문 내역을 상세조회 할 수 있다.")
+    @Test
+    fun getOrder() {
+        val id = 32L
+        val userId = 1L
+        val storeId = 7L
+
+        val request = CreateOrderRequest(
+            storeId = storeId,
+            paymentMethod = PaymentMethod.NAVER_PAY,
+            deliveryType = DeliveryType.OUTSOURCING,
+            zipCode = "12345",
+            address = "서울시 강남구 역삼동",
+            detailAddress = "위워크 빌딩 19층",
+            phoneNumber = "010-1234-5678",
+            messageToRider = "1층 로비에 보관 부탁드립니다",
+            messageToStore = "리뷰이벤트 참여합니다 !",
+            totalPrice = 23000,
+            deliveryFee = 0,
+            orderItems = listOf(
+                CreateOrderItemRequest(menuId = 1L, quantity = 2, optionIds = listOf(1L, 2L)),
+                CreateOrderItemRequest(menuId = 2L, quantity = 1, optionIds = listOf(3L)),
+            )
+        )
+        val now = LocalDateTime.now()
+        val order = generateOrder(id, userId, request, now)
+        val orderItems = generateOrderItems(now)
+        val orderItemOptions = generateOrderItemOptions(now)
+
+        coEvery { useCase.getOrder(any(), any()) } returns order
+        coEvery { orderItemUseCase.getAllByOrderId(any()) } returns orderItems
+        coEvery { orderItemOptionUseCase.getAllByOrderItemIdIn(any()) } returns orderItemOptions
+
+        val orderItemResponses = orderItems.map { orderItem ->
+            val itemOptions = orderItemOptions.filter { it.orderItemId == orderItem.id }
+                .map { OrderItemOptionResponse.from(it) }
+            OrderItemResponse.from(orderItem, itemOptions)
+        }
+        val expectedResponse = OrderResponse.from(order, orderItemResponses)
+        val expectedTotalPrice = orderItems.sumOf { it.menuPrice } + orderItemOptions.sumOf { it.optionPrice }
+
+        webTestClient.get().uri("/api/v1/users/$userId/orders/${order.id}")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .consumeWith(::println)
+            .jsonPath("$.id").isEqualTo(expectedResponse.id)
+            .jsonPath("$.orderNumber").isEqualTo(expectedResponse.orderNumber)
+            .jsonPath("$.userId").isEqualTo(expectedResponse.userId)
+            .jsonPath("$.storeId").isEqualTo(expectedResponse.storeId)
+            .jsonPath("$.paymentId").isEqualTo(expectedResponse.paymentId)
+            .jsonPath("$.paymentMethod").isEqualTo(expectedResponse.paymentMethod.name)
+            .jsonPath("$.status").isEqualTo(expectedResponse.status.name)
+            .jsonPath("$.deliveryFee").isEqualTo(expectedResponse.deliveryFee)
+            .jsonPath("$.zipCode").isEqualTo(expectedResponse.zipCode)
+            .jsonPath("$.address").isEqualTo(expectedResponse.address)
+            .jsonPath("$.detailAddress").isEqualTo(expectedResponse.detailAddress)
+            .jsonPath("$.phoneNumber").isEqualTo(expectedResponse.phoneNumber)
+            .jsonPath("$.messageToRider").isEqualTo(expectedResponse.messageToRider!!)
+            .jsonPath("$.messageToStore").isEqualTo(expectedResponse.messageToStore!!)
+            .jsonPath("$.totalPrice").isEqualTo(expectedResponse.totalPrice)
+            .jsonPath("$.deliveryFee").isEqualTo(expectedResponse.deliveryFee)
+            .jsonPath("$.orderItems").isArray
+            .jsonPath("$.orderItems[0].id").isEqualTo(expectedResponse.orderItems[0].id)
+            .jsonPath("$.orderItems[0].menuId").isEqualTo(expectedResponse.orderItems[0].menuId)
+            .jsonPath("$.orderItems[0].menuName").isEqualTo(expectedResponse.orderItems[0].menuName)
+            .jsonPath("$.orderItems[0].price").isEqualTo(expectedResponse.orderItems[0].price)
+            .jsonPath("$.orderItems[0].quantity").isEqualTo(expectedResponse.orderItems[0].quantity)
+            .jsonPath("$.orderItems[0].options").isArray
+            .jsonPath("$.orderItems[0].options[0].id").isEqualTo(expectedResponse.orderItems[0].options[0].id)
+            .jsonPath("$.orderItems[0].options[0].orderItemId")
+            .isEqualTo(expectedResponse.orderItems[0].options[0].orderItemId)
+            .jsonPath("$.orderItems[0].options[0].optionId")
+            .isEqualTo(expectedResponse.orderItems[0].options[0].optionId)
+            .jsonPath("$.orderItems[0].options[0].optionName")
+            .isEqualTo(expectedResponse.orderItems[0].options[0].optionName)
+            .jsonPath("$.orderItems[0].options[0].price").isEqualTo(expectedResponse.orderItems[0].options[0].price)
+            .jsonPath("$.orderItems[0].options[1].id").isEqualTo(expectedResponse.orderItems[0].options[1].id)
+            .jsonPath("$.orderItems[0].options[1].orderItemId")
+            .isEqualTo(expectedResponse.orderItems[0].options[1].orderItemId)
+            .jsonPath("$.orderItems[0].options[1].optionId")
+            .isEqualTo(expectedResponse.orderItems[0].options[1].optionId)
+            .jsonPath("$.orderItems[0].options[1].optionName")
+            .isEqualTo(expectedResponse.orderItems[0].options[1].optionName)
+            .jsonPath("$.orderItems[0].options[1].price").isEqualTo(expectedResponse.orderItems[0].options[1].price)
+            .jsonPath("$.totalPrice").isEqualTo(expectedTotalPrice)
+    }
+
     private fun generateOrder(id: Long, userId: Long, request: CreateOrderRequest, now: LocalDateTime): Order {
         val orderNumber = generateOrderNumber(now)
         val paymentId = generatePaymentId()
