381e2302e21c8c9f3957adf8953636a2f894428a
hyuuny <shyune@knou.ac.kr>
2024-06-28 06:37:36 +0900

    test(admin-api): 리뷰 상세조회 기능 테스트 추가

--- src/test/kotlin/hyuuny/fooddelivery/presentation/admin/v1/review/ReviewHandlerTest.kt
+++ src/test/kotlin/hyuuny/fooddelivery/presentation/admin/v1/review/ReviewHandlerTest.kt
@@ -1,14 +1,19 @@
 package hyuuny.fooddelivery.presentation.admin.v1.review
 
 import com.ninjasquad.springmockk.MockkBean
+import hyuuny.fooddelivery.application.order.OrderItemUseCase
+import hyuuny.fooddelivery.application.review.ReviewPhotoUseCase
 import hyuuny.fooddelivery.application.review.ReviewUseCase
 import hyuuny.fooddelivery.application.store.StoreUseCase
 import hyuuny.fooddelivery.application.user.UserUseCase
 import hyuuny.fooddelivery.common.constant.DeliveryType
+import hyuuny.fooddelivery.domain.order.OrderItem
 import hyuuny.fooddelivery.domain.review.Review
+import hyuuny.fooddelivery.domain.review.ReviewPhoto
 import hyuuny.fooddelivery.domain.store.Store
 import hyuuny.fooddelivery.domain.user.User
 import hyuuny.fooddelivery.presentation.admin.v1.BaseIntegrationTest
+import hyuuny.fooddelivery.presentation.admin.v1.review.response.ReviewResponse
 import io.mockk.coEvery
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -24,10 +29,16 @@ class ReviewHandlerTest : BaseIntegrationTest() {
     private lateinit var useCase: ReviewUseCase
 
     @MockkBean
+    private lateinit var reviewPhotoUseCase: ReviewPhotoUseCase
+
+    @MockkBean
     private lateinit var userUseCase: UserUseCase
 
     @MockkBean
     private lateinit var storeUseCase: StoreUseCase
+
+    @MockkBean
+    private lateinit var orderItemUseCase: OrderItemUseCase
 
     @DisplayName("관리자는 회원들이 작성한 리뷰 목록을 조회할 수 있다.")
     @Test
@@ -142,6 +153,94 @@ class ReviewHandlerTest : BaseIntegrationTest() {
             .jsonPath("$.size").isEqualTo(15)
             .jsonPath("$.last").isEqualTo(true)
             .jsonPath("$.totalElements").isEqualTo(4)
+    }
+
+    @DisplayName("관리자는 리뷰 상세조회를 할 수 있다.")
+    @Test
+    fun getReview() {
+        val reviewId = 1L
+        val userId = 84L
+        val storeId = 52L
+        val orderId = 1938L
+
+        val now = LocalDateTime.now()
+        val user = User(
+            id = userId,
+            name = "김성현",
+            nickname = "hyuuny",
+            email = "shyune@knou.ac.kr",
+            phoneNumber = "010-1234-1234",
+            imageUrl = "https://my-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/hyuuny.jpeg",
+            createdAt = now,
+            updatedAt = now,
+        )
+        val store = Store(
+            id = storeId,
+            categoryId = 2L,
+            deliveryType = DeliveryType.OUTSOURCING,
+            name = "백종원의 빽보이피자",
+            ownerName = "나피자",
+            taxId = "125-21-38923",
+            deliveryFee = 0,
+            minimumOrderAmount = 14000,
+            iconImageUrl = "icon-image-url-1.jpg",
+            description = "안녕하세요. 백종원이 빽보이피자입니다 :)\n" +
+                    " ★ 음료는 기본 제공되지 않습니다. 필요하신분은 추가 주문 부탁드립니다.\n" +
+                    " ★ 다양한 리뷰이베트는 리뷰칸을 확인해주세요!",
+            foodOrigin = "슈퍼빽보이'카나디언:돼지고기(국내산), 페퍼로니: 돼지고기(국내산과 외국산 섞음), 소고기(호주산), 베이컨:돼지고기(미국산)",
+            phoneNumber = "02-1231-2308",
+            createdAt = now.minusYears(1),
+            updatedAt = now.minusYears(1),
+        )
+        val review = Review(
+            id = reviewId,
+            userId = userId,
+            storeId = storeId,
+            orderId = orderId,
+            score = 5,
+            content = "맛있어요. 다음에 또 주문할게요.",
+            createdAt = now,
+        )
+        val reviewPhotos = listOf(
+            ReviewPhoto(1L, reviewId, "https://my-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/review-1.jpg", now),
+            ReviewPhoto(2L, reviewId, "https://my-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/review-2.jpg", now),
+            ReviewPhoto(3L, reviewId, "https://my-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/review-3.jpg", now),
+        )
+        val orderItems = listOf(
+            OrderItem(5L, orderId, 5L, "아메리카노", 4000, 4, now),
+            OrderItem(6L, orderId, 7L, "카페라떼", 5000, 3, now),
+            OrderItem(7L, orderId, 13L, "스콘", 3000, 1, now),
+        )
+
+        coEvery { useCase.getReview(any()) } returns review
+        coEvery { reviewPhotoUseCase.getAllByReviewId(any()) } returns reviewPhotos
+        coEvery { userUseCase.getUser(any()) } returns user
+        coEvery { storeUseCase.getStore(any()) } returns store
+        coEvery { orderItemUseCase.getAllByOrderId(any()) } returns orderItems
+
+        val expectedResponse = ReviewResponse.from(review, user, store, orderItems, reviewPhotos)
+        webTestClient.get().uri("/admin/v1/reviews/$reviewId")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .consumeWith(::println)
+            .jsonPath("$.id").isEqualTo(expectedResponse.id)
+            .jsonPath("$.userId").isEqualTo(expectedResponse.userId)
+            .jsonPath("$.userNickname").isEqualTo(expectedResponse.userNickname)
+            .jsonPath("$.userImageUrl").isEqualTo(expectedResponse.userImageUrl!!)
+            .jsonPath("$.storeId").isEqualTo(expectedResponse.storeId)
+            .jsonPath("$.storeName").isEqualTo(expectedResponse.storeName)
+            .jsonPath("$.orderId").isEqualTo(expectedResponse.orderId)
+            .jsonPath("$.score").isEqualTo(expectedResponse.score)
+            .jsonPath("$.content").isEqualTo(expectedResponse.content)
+            .jsonPath("$.items[0].menuName").isEqualTo(expectedResponse.items[0].menuName)
+            .jsonPath("$.items[1].menuName").isEqualTo(expectedResponse.items[1].menuName)
+            .jsonPath("$.items[2].menuName").isEqualTo(expectedResponse.items[2].menuName)
+            .jsonPath("$.photos[0].photoUrl").isEqualTo(expectedResponse.photos[0].photoUrl)
+            .jsonPath("$.photos[1].photoUrl").isEqualTo(expectedResponse.photos[1].photoUrl)
+            .jsonPath("$.createdAt").exists()
+
     }
 
 }
