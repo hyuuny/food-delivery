a36a6ab60004cf3c4b61ba0b83e8ce46d857daf1
hyuuny <shyune@knou.ac.kr>
2024-07-06 00:31:57 +0900

    feat(api): Menu API ResponseMapper 구현

--- src/main/kotlin/hyuuny/fooddelivery/presentation/api/v1/menu/MenuApiHandler.kt
+++ src/main/kotlin/hyuuny/fooddelivery/presentation/api/v1/menu/MenuApiHandler.kt
@@ -1,14 +1,6 @@
 package hyuuny.fooddelivery.presentation.api.v1.menu
 
 import hyuuny.fooddelivery.application.menu.MenuUseCase
-import hyuuny.fooddelivery.application.option.OptionUseCase
-import hyuuny.fooddelivery.application.optiongroup.OptionGroupUseCase
-import hyuuny.fooddelivery.domain.option.Option
-import hyuuny.fooddelivery.presentation.api.v1.menu.response.MenuResponse
-import hyuuny.fooddelivery.presentation.api.v1.menu.response.OptionGroupResponse
-import hyuuny.fooddelivery.presentation.api.v1.menu.response.OptionResponse
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -18,34 +10,15 @@ import org.springframework.web.reactive.function.server.bodyValueAndAwait
 @Component
 class MenuApiHandler(
     private val useCase: MenuUseCase,
-    private val optionGroupUseCase: OptionGroupUseCase,
-    private val optionUseCase: OptionUseCase,
+    private val responseMapper: MenuResponseMapper,
 ) {
 
     suspend fun getMenu(request: ServerRequest): ServerResponse {
         val id = request.pathVariable("id").toLong()
 
-        return coroutineScope {
-            val menuDeferred = async { useCase.getMenu(id) }
-            val optionGroupsDeferred = async { optionGroupUseCase.getAllByMenuId(id).sortedBy { it.priority } }
-
-            val optionGroups = optionGroupsDeferred.await()
-            val optionsDeferred = async { optionUseCase.getAllByOptionGroupIds(optionGroups.mapNotNull { it.id }) }
-
-            val menu = menuDeferred.await()
-            val options = optionsDeferred.await()
-            val optionMap = options
-                .sortedWith(compareBy<Option> { it.price }.thenByDescending { it.id })
-                .groupBy { it.optionGroupId }
-
-            val optionGroupResponse = optionGroups.map { group ->
-                val optionsOfOptionGroup = optionMap[group.id]?.map { OptionResponse.from(it) } ?: emptyList()
-                OptionGroupResponse.from(group, optionsOfOptionGroup)
-            }
-
-            val response = MenuResponse.from(menu, optionGroupResponse)
-            ok().bodyValueAndAwait(response)
-        }
+        val menu = useCase.getMenu(id)
+        val response = responseMapper.mepToMenuResponse(menu)
+        return ok().bodyValueAndAwait(response)
     }
 
 }
